c1d5d9f07955e4f4eb3d244f1606189cb4099fc4
Vivien Gondal <v.gondal@crescendo-restauration.fr>
2022-06-20 09:33:54 +0200

    Add element for integration bootstrap

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,28 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import babel from '@rollup/plugin-babel';
 export default [
     {
         input: 'src/index.js',
         output: {
             file: 'js/main.js',
-            format: 'iife',
+            format: 'iife'
         },
-        plugins: [nodeResolve()]
+        plugins: [
+            babel({
+                exclude: 'node_modules/**',
+                babelHelpers: 'bundled'
+            }),
+            replace({
+                values : {
+                    'process.env.NODE_ENV': JSON.stringify('production'),
+                    '// eslint-disable-next-line import/no-unused-modules': '',
+                    'offsetParent = offsetParent;': ''
+                },
+                delimiters: ['', ''],
+                preventAssignment: true
+            }),
+            nodeResolve()
+        ]
     }
 ];
