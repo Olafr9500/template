c4f2cffb0282b29c40be5b9f7a209ff79bcfb227
Vivien Gondal <v.gondal@crescendo-restauration.fr>
2022-06-20 09:36:49 +0200

    Add console log for server js

--- index.js
+++ index.js
@@ -2,16 +2,28 @@
 const http = require('http');
 const fs = require('fs');
 
+console.clear();
+
 http.createServer(function (req, res) {
-    fs.readFile(__dirname + req.url, function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
-            return;
-        }
-        res.writeHead(200);
-        res.end(data);
-    });
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.readFile('index.html', function (err, data) {
+            console.log(`${req.method} ${req.url} - 200`);
+            res.end(data);
+        });
+    } else {
+        fs.readFile(__dirname + req.url, function (err, data) {
+            if (err) {
+                res.writeHead(404);
+                res.end(JSON.stringify(err));
+                console.log(`${req.method} ${req.url} - 404 ${err.message}`);
+                return;
+            }
+            res.writeHead(200);
+            console.log(`${req.method} ${req.url} - 200`);
+            res.end(data);
+        });
+    }
 }).listen(3000, () => {
     console.log('Server running at http://localhost:3000/');
 });
