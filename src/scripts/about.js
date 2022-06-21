286208f412cfecc397f69364e4363028ef8f98b7
Vivien Gondal <v.gondal@crescendo-restauration.fr>
2022-06-21 16:36:01 +0200

    Init page about

--- src/scripts/about.js
+++ src/scripts/about.js
@@ -1,4 +1,24 @@
-
+import { docQAll } from '@olafr/modulejs';
+import Collapse from 'bootstrap/js/dist/collapse';
+import callSW from './sw';
 window.addEventListener('load', () => {
-    console.log('load');
+    console.log('Loaded');
+    callSW();
+
+    let collapseElementList = [].slice.call(docQAll('.collapse')),
+        collapseList = collapseElementList.map(function (collapseEl) {
+            return new Collapse(collapseEl, {
+                toggle: false
+            });
+        });
+
+    collapseElementList.forEach(collapseEl => {
+        collapseEl.addEventListener('click', () => {
+            collapseList.forEach(collapse => {
+                if (collapse === collapseEl) {
+                    collapse.toggle();
+                }
+            });
+        });
+    });
 });
