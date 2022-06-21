99ed8ace4ad504f093cc23add6b34e1a9e963edf
Vivien Gondal <v.gondal@crescendo-restauration.fr>
2022-06-21 10:13:27 +0200

    Update server

--- index.js
+++ index.js
@@ -5,24 +5,104 @@ const fs = require('fs');
 console.clear();
 
 http.createServer(function (req, res) {
-    if (req.url === '/') {
+    const url = req.url.split('?')[0];
+    switch (url) {
+    case '/':
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        fs.readFile('index.html', function (err, data) {
+        fs.readFile('public/pages/index.html', function (err, data) {
+            if (err) {
+                res.setHeader('Content-Type', 'application/json');
+                res.writeHead(404);
+                res.end(JSON.stringify(err));
+                console.log(`${req.method} ${req.url} - 404 ${err}`);
+                return;
+            }
             console.log(`${req.method} ${req.url} - 200`);
             res.end(data);
         });
-    } else {
-        fs.readFile(__dirname + req.url, function (err, data) {
+        break;
+    case '/about':
+    case '/about/':
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.readFile('public/pages/about.html', function (err, data) {
+            if (err) {
+                res.setHeader('Content-Type', 'application/json');
+                res.writeHead(404);
+                res.end(JSON.stringify(err));
+                console.log(`${req.method} ${req.url} - 404 ${err}`);
+                return;
+            }
+            console.log(`${req.method} ${req.url} - 200`);
+            res.end(data);
+        });
+        break;
+    case '/package.json':
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        fs.readFile('package.json', function (err, data) {
             if (err) {
                 res.writeHead(404);
                 res.end(JSON.stringify(err));
-                console.log(`${req.method} ${req.url} - 404 ${err.message}`);
+                console.log(`${req.method} ${req.url} - 404 ${err}`);
                 return;
             }
-            res.writeHead(200);
             console.log(`${req.method} ${req.url} - 200`);
             res.end(data);
         });
+        break;
+    default:
+        if (url.startsWith('/css')) {
+            res.writeHead(200, { 'Content-Type': 'text/css' });
+            fs.readFile('public' + url, function (err, data) {
+                if (err) {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.writeHead(404);
+                    res.end(JSON.stringify(err));
+                    console.log(`${req.method} ${req.url} - 404 ${err}`);
+                    return;
+                }
+                console.log(`${req.method} ${req.url} - 200`);
+                res.end(data);
+            });
+        } else if (url.startsWith('/js')) {
+            res.writeHead(200, { 'Content-Type': 'application/javascript' });
+            fs.readFile('public' + url, function (err, data) {
+                if (err) {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.writeHead(404);
+                    res.end(JSON.stringify(err));
+                    console.log(`${req.method} ${req.url} - 404 ${err}`);
+                    return;
+                }
+                console.log(`${req.method} ${req.url} - 200`);
+                res.end(data);
+            });
+        } else if (url.startsWith('/img')) {
+            res.writeHead(200, { 'Content-Type': 'image/png' });
+            fs.readFile('public' + url, function (err, data) {
+                if (err) {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.writeHead(404);
+                    res.end(JSON.stringify(err));
+                    console.log(`${req.method} ${req.url} - 404 ${err}`);
+                    return;
+                }
+                console.log(`${req.method} ${req.url} - 200`);
+                res.end(data);
+            });
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            fs.readFile('public/pages/404.html', function (err, data) {
+                if (err) {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.writeHead(404);
+                    res.end(JSON.stringify(err));
+                    console.log(`${req.method} ${req.url} - 404 ${err}`);
+                    return;
+                }
+                console.log(`${req.method} ${req.url} - 200`);
+                res.end(data);
+            });
+        }
     }
 }).listen(3000, () => {
     console.log('Server running at http://localhost:3000/');
--- index.php
+++ index.php
@@ -2,4 +2,16 @@
 // if ((!isset($_SERVER['HTTPS'])) || ($_SERVER['HTTPS'] != 'on')) {
 //     header("Location:https://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);
 // }
-include_once 'index.html';
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+    switch ($page) {
+        case 'home':
+            include 'public/pages/index.html';
+            break;
+        case 'about':
+            include 'public/pages/about.html';
+            break;
+    }
+} else {
+    include_once 'public/pages/index.html';
+}
