af594fe6c9fc47203240ef979845bde9a1262287
Olafr9500 <olafr.af.munso@gmail.com>
2022-06-24 11:41:41 +0200

    Correction configuration cypress

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
     e2e: {
-        baseUrl: 'https://example.cypress.io',
+        baseUrl: 'http://localhost:3000',
     },
 });
