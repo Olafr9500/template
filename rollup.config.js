470b985e5a8b37676f454f83e1ed7464d369a371
Olafr9500 <olafr.af.munso@gmail.com>
2022-07-04 13:13:42 +0200

    Better compiler

--- rollup.config.js
+++ rollup.config.js
@@ -2,43 +2,32 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import { babel } from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
+import fs from 'fs';
 
 const scriptsFolder = 'src/scripts/';
-const rollupConfig = [
-    {
-        input: scriptsFolder + 'index.js',
-        output: {
-            file: 'public/js/main.js',
-            format: 'iife'
-        },
-        plugins: [
-            babel({
-                exclude: 'node_modules/**',
-                babelHelpers: 'bundled'
-            }),
-            commonjs(),
-            resolve({
-                browser: true,
-            })
-        ]
-    }, {
-        input: scriptsFolder + 'about.js',
-        output: {
-            file: 'public/js/about.js',
-            format: 'iife'
-        },
-        plugins: [
-            babel({
-                exclude: 'node_modules/**',
-                babelHelpers: 'bundled'
-            }),
-            commonjs(),
-            resolve({
-                browser: true
-            })
-        ]
+const rollupConfig = [];
+fs.readdirSync(scriptsFolder).forEach(file => {
+    if (file.endsWith('.js')) {
+        const name = file.replace('.js', '');
+        rollupConfig.push({
+            input: `${scriptsFolder}${name}.js`,
+            output: {
+                file: `public/scripts/${name}.js`,
+                format: 'iife',
+            },
+            plugins: [
+                babel({
+                    exclude: 'node_modules/**',
+                    babelHelpers: 'bundled'
+                }),
+                commonjs(),
+                resolve({
+                    browser: true,
+                })
+            ]
+        });
     }
-];
+});
 // eslint-disable-next-line no-undef
 if (process.env.production === 'true') {
     rollupConfig.forEach(config => {
